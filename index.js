a916dc74420da884a5d8ed649d0e9420ea1664a0
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-01-28 14:27:50 +0100

    init

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const discord = require("discord.js");
 const botconfig = require("./botconfig");
 const levelFile = require("./data/levels.json")
 const fs = require("fs");
-const ms = require("ms");
 
 const bot = new discord.Client();
 bot.commands = new discord.Collection();

6cc5bbe15bc08ed7862f550143a779f795758c16
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-01-28 11:36:42 +0100

    init

--- commands/dm.js
+++ commands/dm.js
@@ -6,6 +6,8 @@ module.exports.run = async (bot, message, args) => {
 
     if (!user) return message.channel.send("Speler niet gevonden.");
 
+    if(!user.hasPermission("MANAGE_MESSAGES")) return message.channel.send("je moet staff zijn om dit te kunnen doen!")
+
     if(!args[1]) return message.channels.send("Geef ook een bericht mee!");
 
     var bericht = args.join(" ");

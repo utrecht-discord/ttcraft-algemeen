c24da80c8a0656f68ece5cb960c0c1ce1276b474
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-01-28 11:46:05 +0100

    init

--- commands/dm.js
+++ commands/dm.js
@@ -2,15 +2,13 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var author = message.author; 
-
-    if(!author.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Je moet staff zijn om dit te kunnen doen!");
-
     var user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
     if (!user) return message.channel.send("Speler niet gevonden.");
 
-    if(!args[1]) return message.channels.send("Geef ook een bericht mee!");
+    if(!args[1]){
+        return message.channels.send("Geef ook een bericht mee!");
+    } else message.channel.send("Het bericht is verstuurd naar ", user);
 
     var bericht = args.join(" ");
 
@@ -18,6 +16,7 @@ module.exports.run = async (bot, message, args) => {
 
     user.send(bericht);
 
+    
 
 }
 

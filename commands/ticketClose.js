ddba4a9e82b725681551abde9c409cb2a024a997
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-02-04 22:10:30 +0100

    ticketClose.js

--- commands/ticketClose.js
+++ commands/ticketClose.js
@@ -3,11 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
  
     // Id van category van tickets.
-    const categoryId = `661293576826781706`;
- 
+    const categoryId = `661293576826781706';
+    var argumenten = args.join(" ");
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Jij kunt dit niet doen!");
+    if(!argumenten) return message.channel.send("Geef een reden op om de ticket te sluiten!");
     // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
-    if (message.channel.parentID == categoryId) {
+    if (argumenten, message.channel.parentID == categoryId) {
         
         message.channel.delete();
  
@@ -17,18 +18,15 @@ module.exports.run = async (bot, message, args) => {
  
     }
  
-    var argumenten = args.join(" ");
-    if(!argumenten) return message.channel.send("Geef een reden op om de ticket te sluiten!");
-
     var embedCloseTicket = new discord.RichEmbed()
         .setTitle("Hoi, " + message.channel.name)
-        .setDescription("Je ticket is **__AFGEHANDELD__**. \nWil je een nieuwe maken doe dan !ticket")
+        .setDescription("Je ticket is **__GESLOTEN__**. Wil je een nieuwe maken doe dan !ticket")
         .setColor("#FF0000")
         .addField("Reden van sluiting: ", argumenten)
         .setFooter("ticket gesloten");
  
     // Vind kanaal voor de logs.
-    var logChannel = message.guild.channels.find("name", "logs");
+    var logChannel = message.guild.channels.find("name", "ticket-logs");
     if (!logChannel) return message.channel.send("Kanaal bestaat niet");
  
     logChannel.send(embedCloseTicket);
